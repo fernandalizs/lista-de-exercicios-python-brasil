cbd975addbdc7a7f0d810fd9c8dcd08e9bea5f00
Fernanda Liz <fernandaliz2004@hotmail.com>
2022-05-24 13:34:18 -0300

    solucao ex_02_escreva_um_numero

--- secao_01_estrutura_sequencial/ex_02_escreva_um_numero.py
+++ secao_01_estrutura_sequencial/ex_02_escreva_um_numero.py
@@ -13,4 +13,5 @@ Faça um Programa que peça um número e então mostre a mensagem O número info
 
 def escreva_um_numero():
     """Escreva aqui em baixo a sua solução"""
-
+    n = (input('42'))
+    print(f'O número informado foi {n}')
